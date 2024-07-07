8c57f6628525b6bb9b31bfa2b6aa60490e629837
patrykluszcz <patryk.luszcz@microsoft.wsei.edu.pl>
2024-07-07 17:39:48 +0200

    feat: implement why flight with us section for mobile views

--- frontend/projects/website/src/app/app.component.ts
+++ frontend/projects/website/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { MainComponent } from './components/main/main.component';
 import { OfferSectionComponent } from './components/offer-section/offer-section.component';
+import { WhyUsSectionComponent } from './components/why-us-section/why-us-section.component';
 
 @Component({
   selector: 'app-root',
@@ -12,6 +13,7 @@ import { OfferSectionComponent } from './components/offer-section/offer-section.
     NavbarComponent,
     MainComponent,
     OfferSectionComponent,
+    WhyUsSectionComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
