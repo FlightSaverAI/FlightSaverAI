5ab8351312e4b62e76b66545b65fd798690452bd
patrykluszcz <patryk.luszcz@microsoft.wsei.edu.pl>
2024-07-07 12:22:14 +0200

    feat: add app offer section to app component

--- frontend/projects/website/src/app/app.component.ts
+++ frontend/projects/website/src/app/app.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { MainComponent } from './components/main/main.component';
+import { OfferSectionComponent } from './components/offer-section/offer-section.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, MainComponent],
+  imports: [
+    RouterOutlet,
+    NavbarComponent,
+    MainComponent,
+    OfferSectionComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
