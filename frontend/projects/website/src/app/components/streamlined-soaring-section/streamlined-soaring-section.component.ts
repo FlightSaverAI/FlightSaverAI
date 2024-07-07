b1d875e758fb2f73242bd7600d8d57ccca291abe
patrykluszcz <patryk.luszcz@microsoft.wsei.edu.pl>
2024-07-07 17:46:26 +0200

    feat: create streamlined soaring section component

---
+++ frontend/projects/website/src/app/components/streamlined-soaring-section/streamlined-soaring-section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-streamlined-soaring-section',
+  standalone: true,
+  imports: [],
+  templateUrl: './streamlined-soaring-section.component.html',
+  styleUrl: './streamlined-soaring-section.component.scss'
+})
+export class StreamlinedSoaringSectionComponent {
+
+}
