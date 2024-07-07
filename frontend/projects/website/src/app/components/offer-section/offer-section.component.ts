172b5233f860d105d6094c509235774e08ebf680
patrykluszcz <patryk.luszcz@microsoft.wsei.edu.pl>
2024-07-07 15:22:58 +0200

    feat: add three new cards and implement slider in offer section

--- frontend/projects/website/src/app/components/offer-section/offer-section.component.ts
+++ frontend/projects/website/src/app/components/offer-section/offer-section.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-offer-section',
   standalone: true,
   imports: [],
   templateUrl: './offer-section.component.html',
-  styleUrl: './offer-section.component.scss'
+  styleUrl: './offer-section.component.scss',
 })
 export class OfferSectionComponent {
+  @ViewChild('cards') cards!: ElementRef<HTMLElement>;
 
+  cardIndex = 0;
+
+  public nextCard() {
+    this.cardIndex = (this.cardIndex + 1) % 4;
+
+    this.cards.nativeElement.style.transform = `translateX(-${this.cardIndex * 100}%)`;
+  }
+
+  public previousCard() {
+    this.cardIndex > 0 ? this.cardIndex-- : this.cardIndex;
+
+    this.cards.nativeElement.style.transform = `translateX(-${this.cardIndex * 100}%)`;
+  }
 }
