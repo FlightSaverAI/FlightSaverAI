70d0d9f674cfcddd1d6e7d0985810aa0846e5a48
patrykluszcz <patryk.luszcz@outlook.com>
2024-09-08 21:15:57 +0200

    feat: install ssr package and configuration + pwa

--- frontend/apps/website/src/app/app.config.ts
+++ frontend/apps/website/src/app/app.config.ts
@@ -3,9 +3,13 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideServiceWorker } from '@angular/service-worker';
+import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    }), provideClientHydration(), provideServiceWorker('ngsw-worker.js', {
         enabled: !isDevMode(),
         registrationStrategy: 'registerWhenStable:30000'
     })]
