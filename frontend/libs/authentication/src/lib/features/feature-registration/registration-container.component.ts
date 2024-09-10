112ffce22f2af7a73a413fde1f0c32791dcda1a7
patrykluszcz <patryk.luszcz@outlook.com>
2024-09-10 23:52:35 +0200

    ref: change component name to registration container component

--- frontend/libs/authentication/src/lib/features/feature-registration/registration-container.component.ts
+++ frontend/libs/authentication/src/lib/features/feature-registration/registration-container.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'auth-registration',
   standalone: true,
   imports: [],
   template: ` <p>registration works!</p> `,
 })
-export class RegistrationComponent {}
+export class RegistrationContainerComponent {}
