e1c0b4f73ec9b459018fdf2c61e41fd17e70799f
patrykluszcz <patryk.luszcz@outlook.com>
2024-09-12 01:23:44 +0200

    feat: create design for input component - change library prefix to shared

--- frontend/libs/shared/ui-components/src/lib/button/button.component.ts
+++ frontend/libs/shared/ui-components/src/lib/button/button.component.ts
@@ -1,13 +1,12 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'lib-button',
+  selector: 'shared-button',
   standalone: true,
   imports: [CommonModule],
   template: `<p>button works!</p>`,
   styleUrl: './button.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ButtonComponent {
-  
-}
+export class ButtonComponent {}
--- frontend/libs/shared/ui-components/src/lib/input/input.component.ts
+++ frontend/libs/shared/ui-components/src/lib/input/input.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
-  selector: 'lib-input',
+  selector: 'shared-input',
   standalone: true,
-  imports: [CommonModule],
-  template: `<div>
-    <input />
-  </div> `,
+  imports: [CommonModule, NgOptimizedImage],
+  template: `
+    <label for="input">{{ label() }}</label>
+    <div id="input" class="input-container">
+      <!-- @if(iconSrc()){ -->
+      <div class="icon-container">
+        <img [ngSrc]="iconSrc()" alt="" [width]="20" [height]="20" />
+      </div>
+      <!-- } -->
+      <input [type]="type()" [placeholder]="placeholder()" />
+    </div>
+  `,
   styleUrl: './input.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class InputComponent {}
+export class InputComponent {
+  label = input<string>('');
+  iconSrc = input<string>('');
+  placeholder = input<string>('');
+  type = input<string>('text');
+}
