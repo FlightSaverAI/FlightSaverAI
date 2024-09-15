2bb3cf2fc191b101731795e2645ab93fe8992027
patrykluszcz <patryk.luszcz@outlook.com>
2024-09-15 17:55:43 +0200

    feat: implement control value accessor in input component

--- frontend/libs/shared/ui-components/src/lib/input/input.component.ts
+++ frontend/libs/shared/ui-components/src/lib/input/input.component.ts
@@ -1,6 +1,7 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, forwardRef, input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgOptimizedImage } from '@angular/common';
+import { ControlValueAccessor, FormGroup, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'shared-input',
@@ -9,20 +10,53 @@ import { NgOptimizedImage } from '@angular/common';
   template: `
     <label for="input">{{ label() }}</label>
     <div id="input" class="input-container">
-      <!-- @if(iconSrc()){ -->
+      @if(iconSrc()){
       <div class="icon-container">
         <img [ngSrc]="iconSrc()" alt="" [width]="20" [height]="20" />
       </div>
-      <!-- } -->
-      <input [type]="type()" [placeholder]="placeholder()" />
+      }
+      <input (input)="setInput($event.target)" [type]="type()" [placeholder]="placeholder()" />
     </div>
   `,
   styleUrl: './input.component.scss',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputComponent),
+      multi: true,
+    },
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class InputComponent {
+export class InputComponent implements ControlValueAccessor {
   label = input<string>('');
   iconSrc = input<string>('');
   placeholder = input<string>('');
   type = input<string>('text');
+  parentForm = input.required<FormGroup<any>>();
+  fieldName = input.required<string>();
+
+  onChange!: <T>(value: T) => void;
+  onTouched!: () => void;
+
+  get formField() {
+    return this.parentForm().get(this.fieldName());
+  }
+
+  writeValue(obj: any): void {
+    console.log(obj);
+  }
+
+  registerOnChange(fn: () => void): void {
+    this.onChange = fn;
+  }
+  registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+
+  setInput(target: EventTarget | null): void {
+    const inputValue = (target as HTMLInputElement).value;
+
+    this.onChange(inputValue);
+  }
 }
