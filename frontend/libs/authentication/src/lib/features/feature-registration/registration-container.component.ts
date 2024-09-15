4f08e9d52a213e102fce865bec499f2b8ccfd62d
patrykluszcz <patryk.luszcz@outlook.com>
2024-09-15 18:45:39 +0200

    feat: add ui registration template to smart component registration

--- frontend/libs/authentication/src/lib/features/feature-registration/registration-container.component.ts
+++ frontend/libs/authentication/src/lib/features/feature-registration/registration-container.component.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
+import { RegistrationComponent } from '../../ui/ui-registration/registration.component';
+import { registrationForm } from '../../utils/registration-form';
 
 @Component({
   standalone: true,
-  imports: [],
-  template: ` <p>registration works!</p> `,
+  imports: [RegistrationComponent],
+  template: `
+    <auth-registration
+      [registrationForm]="registrationForm"
+      (confirmRegistration)="confirmRegistration()"
+    ></auth-registration>
+  `,
 })
-export class RegistrationContainerComponent {}
+export class RegistrationContainerComponent {
+  readonly registrationForm = registrationForm();
+
+  confirmRegistration() {
+    console.log(this.registrationForm.getRawValue());
+  }
+}
