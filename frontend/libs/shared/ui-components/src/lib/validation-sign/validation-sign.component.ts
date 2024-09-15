f1cdec63ae940ed2601416b43fcebbca855843b5
patrykluszcz <patryk.luszcz@outlook.com>
2024-09-16 01:31:31 +0200

    feat: create validation sign component

---
+++ frontend/libs/shared/ui-components/src/lib/validation-sign/validation-sign.component.ts
@@ -0,0 +1,23 @@
+import { Component, input } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'shared-validation-sign',
+  standalone: true,
+  imports: [],
+  template: `<div class="validator">
+    @if(hasValidator && !this.formField().pristine && formField().status === 'INVALID'){
+    <img width="20" height="20" src="global/assets/error.svg" alt="Error Icon" />
+    } @if(hasValidator && !this.formField().pristine && formField().status === 'VALID'){
+    <img width="25" height="25" src="global/assets/pass.svg" alt="Complete Icon" />
+    }
+  </div> `,
+  styleUrl: './validation-sign.component.scss',
+})
+export class ValidationSignComponent {
+  formField = input.required<FormControl>();
+
+  get hasValidator(): boolean {
+    return this.formField() && this.formField().validator ? true : false;
+  }
+}
