473add0f9d33ac5f5241caacde9847f9b879ed7d
patrykluszcz <patryk.luszcz@outlook.com>
2024-09-15 18:44:09 +0200

    feat: add new path route registration

--- frontend/libs/authentication/src/lib/routes/auth.routes.ts
+++ frontend/libs/authentication/src/lib/routes/auth.routes.ts
@@ -1,5 +1,6 @@
 import { Route } from '@angular/router';
 import { LoginContainerComponent } from '../features/feature-login/login-container.component';
+import { RegistrationContainerComponent } from '../features/feature-registration/registration-container.component';
 
 export const authRoutes: Route[] = [
   {
@@ -10,5 +11,9 @@ export const authRoutes: Route[] = [
   {
     path: 'login',
     component: LoginContainerComponent,
+  },
+  {
+    path: 'registration',
+    component: RegistrationContainerComponent,
   },
 ];
