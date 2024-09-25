741e5aef978b291e65438b430479b76602bc41c6
patrykluszcz <patryk.luszcz@outlook.com>
2024-09-26 00:37:00 +0200

    feat: create home features library and home container component

---
+++ frontend/libs/home/features/src/lib/home-container/home-container.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  template: ` <p>home-container works!</p> `,
+  styleUrl: './home-container.component.scss',
+})
+export class HomeContainerComponent {}
