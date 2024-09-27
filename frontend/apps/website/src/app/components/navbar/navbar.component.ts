8e152cc3cddcc47dd8a0d0de4f8c7d38c5d371b2
patrykluszcz <patryk.luszcz@outlook.com>
2024-09-28 00:28:59 +0200

    ref: remove button not existing button mixin + replace it by shared button component

--- frontend/apps/website/src/app/components/navbar/navbar.component.ts
+++ frontend/apps/website/src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NgClass } from '@angular/common';
+import { ButtonComponent } from '@shared/ui-components';
 
 @Component({
   selector: 'web-navbar',
   standalone: true,
-  imports: [NgClass],
+  imports: [NgClass, ButtonComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
