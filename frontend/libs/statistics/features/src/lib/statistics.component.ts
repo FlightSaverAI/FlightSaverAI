dfd5eeb8cd70415f3cdc9986e69ac726153710a9
patrykluszcz <patryk.luszcz@outlook.com>
2024-10-05 01:38:33 +0200

    feat: create features library in statistics domain

---
+++ frontend/libs/statistics/features/src/lib/statistics.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+// import { NgxEchartsDirective, provideEcharts } from 'ngx-echarts';
+// import { EChartsOption } from 'echarts/types/dist/echarts';
+
+@Component({
+  standalone: true,
+  imports: [CommonModule],
+  template: ``,
+  styleUrl: './statistics.component.scss',
+  providers: [],
+})
+export class StatisticsComponent {}
--- frontend/libs/statistics/routes/src/lib/statistics.routes.ts
+++ frontend/libs/statistics/routes/src/lib/statistics.routes.ts
@@ -1,7 +1,9 @@
 import { Route } from '@angular/router';
+import { StatisticsComponent } from '@flight-saver/statistics/features';
 
 export const statisticsRoutes: Route[] = [
   {
     path: '',
+    component: StatisticsComponent,
   },
 ];
