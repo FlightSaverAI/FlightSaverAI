2a3ba8eb8bf5247d9e5867dec333fd8ba3ef7b8e
patrykluszcz <patryk.luszcz@outlook.com>
2024-10-08 00:21:26 +0200

    feat: create shared slider card component

--- frontend/libs/shared/ui-components/src/index.ts
+++ frontend/libs/shared/ui-components/src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/input/input.component';
 export * from './lib/button/button.component';
 export * from './lib/navbar/navbar.component';
+export * from './lib/slider-card/slider-card.component';
