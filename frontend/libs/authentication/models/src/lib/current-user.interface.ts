81eeef67e6ba5820ce3a312d4b5d9c103e4a6f44
patrykluszcz <patryk.luszcz@outlook.com>
2024-11-02 18:47:22 +0100

    ref: remove comment

--- frontend/libs/authentication/models/src/lib/current-user.interface.ts
+++ frontend/libs/authentication/models/src/lib/current-user.interface.ts
@@ -2,5 +2,5 @@ export interface CurrentUser {
   id: number;
   username: string;
   email: string;
-  userRole: string; // this property will need a specific type with roles (temporary solution);
+  userRole: string;
 }
