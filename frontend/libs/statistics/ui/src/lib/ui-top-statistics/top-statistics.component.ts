d9d3658c7dab514c08309a58946654b6601f8366
patrykluszcz <patryk.luszcz@outlook.com>
2024-11-02 18:52:20 +0100

    feat: implemnent logic and design for top statistics

--- frontend/libs/statistics/ui/src/lib/ui-top-statistics/top-statistics.component.ts
+++ frontend/libs/statistics/ui/src/lib/ui-top-statistics/top-statistics.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SliderCardComponent } from '@shared/ui-components';
+import { NgxEchartsDirective, provideEcharts } from 'ngx-echarts';
 
 @Component({
   selector: 'statistics-top-statistics',
   standalone: true,
-  imports: [CommonModule],
-  template: `<p>top-statistics works!</p>`,
+  imports: [CommonModule, SliderCardComponent, NgxEchartsDirective],
+  template: ` <shared-slider-card style="display: flex; justify-content: center; margin-top: 50px">
+    <div class="card" *ngFor="let statistic of topStatistics()">
+      <div class="card__image-container">
+        <img class="card__image" [src]="statistic.imageSrc" alt="" />
+        <div class="card__title">
+          <p>{{ statistic.cardTitle }}</p>
+        </div>
+      </div>
+      <div class="card__info-container">
+        <div echarts [options]="statistic"></div>
+      </div>
+    </div>
+  </shared-slider-card>`,
   styleUrl: './top-statistics.component.scss',
+  providers: [provideEcharts()],
 })
-export class TopStatisticsComponent {}
+export class TopStatisticsComponent {
+  topStatistics = input.required<any>();
+  // options = input.required();
+}
