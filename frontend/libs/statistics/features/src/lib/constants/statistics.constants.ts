17995c8ffd00f46e11e2f1edb10ead63f3c643c2
patrykluszcz <patryk.luszcz@outlook.com>
2024-11-03 01:33:01 +0100

    feat: create configurations for fligth per week and month

--- frontend/libs/statistics/features/src/lib/constants/statistics.constants.ts
+++ frontend/libs/statistics/features/src/lib/constants/statistics.constants.ts
@@ -417,4 +417,120 @@ export abstract class StatisticsContants {
       },
     },
   ];
+
+  static readonly activityOverview = [
+    {
+      cardTitle: 'FLIGHT PER MONTH',
+      imageSrc: 'global/assets/images/activity-per-month.jpeg',
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+          type: 'line',
+        },
+      },
+      xAxis: {
+        type: 'category',
+        data: ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'],
+        axisLabel: {
+          color: '#FFFFFF',
+          interval: 0,
+        },
+        axisLine: {
+          lineStyle: {
+            color: '#FFFFFF',
+          },
+        },
+      },
+      yAxis: {
+        type: 'value',
+        name: 'Flights',
+        axisLabel: {
+          color: '#FFFFFF',
+        },
+        axisLine: {
+          lineStyle: {
+            color: '#FFFFFF',
+          },
+        },
+        splitLine: {
+          lineStyle: {
+            color: 'transparent',
+          },
+        },
+      },
+      series: [
+        {
+          name: 'Flights',
+          type: 'line',
+          smooth: true,
+          data: [3, 0, 1, 2, 3, 4, 3, 2, 0, 1, 1, 0],
+          itemStyle: {
+            color: 'white',
+          },
+          lineStyle: {
+            width: 2,
+          },
+          symbol: 'circle',
+          symbolSize: 8,
+        },
+      ],
+    },
+    {
+      cardTitle: 'FLIGHT PER WEEK',
+      imageSrc: 'global/assets/images/activity-per-week.jpeg',
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+          type: 'line',
+        },
+      },
+      xAxis: {
+        type: 'category',
+        data: ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN'],
+        axisLabel: {
+          color: '#FFFFFF',
+          interval: 0,
+        },
+        axisLine: {
+          lineStyle: {
+            color: '#FFFFFF',
+          },
+        },
+        boundaryGap: false,
+      },
+      yAxis: {
+        type: 'value',
+        name: 'Flights',
+        axisLabel: {
+          color: '#FFFFFF',
+        },
+        axisLine: {
+          lineStyle: {
+            color: '#FFFFFF',
+          },
+        },
+        splitLine: {
+          lineStyle: {
+            color: 'transparent',
+          },
+        },
+      },
+      series: [
+        {
+          name: 'Flights',
+          type: 'line',
+          smooth: true,
+          data: [3, 0, 1, 2, 3, 4, 3, 2, 0, 1, 1, 0],
+          itemStyle: {
+            color: 'white',
+          },
+          lineStyle: {
+            width: 2,
+          },
+          symbol: 'circle',
+          symbolSize: 8,
+        },
+      ],
+    },
+  ];
 }
--- frontend/libs/statistics/features/src/lib/statistics.component.ts
+++ frontend/libs/statistics/features/src/lib/statistics.component.ts
@@ -4,17 +4,27 @@ import { CommonModule } from '@angular/common';
 import { FlightPassengerOverviewComponent } from '@flight-saver/statistics/ui';
 import { TopOverviewComponent } from '@flight-saver/statistics/ui';
 import { StatisticsContants } from './constants/statistics.constants';
+import { ActivityOverviewComponent } from '@flight-saver/statistics/ui';
 
 @Component({
   standalone: true,
-  imports: [CommonModule, FlightPassengerOverviewComponent, TopOverviewComponent],
+  imports: [
+    CommonModule,
+    FlightPassengerOverviewComponent,
+    TopOverviewComponent,
+    ActivityOverviewComponent,
+  ],
   template: `<statistics-flight-passenger-overview
       [flightPassangerOverview]="flightPassangerOverview"
     ></statistics-flight-passenger-overview>
-    <statistics-top-overview [topOverview]="topOverview"></statistics-top-overview> `,
+    <statistics-top-overview [topOverview]="topOverview"></statistics-top-overview>
+    <statistics-activity-overview
+      [activityOverview]="activityOverview"
+    ></statistics-activity-overview>`,
   styleUrl: './statistics.component.scss',
 })
 export class StatisticsComponent {
   flightPassangerOverview = StatisticsContants.flightPassangerOverview;
   topOverview = StatisticsContants.topOverview;
+  activityOverview = StatisticsContants.activityOverview;
 }
