43a6b421329374e65d29dd5d97d27f736369951c
patrykluszcz <patryk.luszcz@outlook.com>
2024-11-03 01:31:51 +0100

    feat: create activity overview component

--- frontend/libs/statistics/ui/src/index.ts
+++ frontend/libs/statistics/ui/src/index.ts
@@ -1,2 +1,3 @@
 export * from './lib/ui-top-overview/top-overview.component';
 export * from './lib/ui-flight-passenger-overview/flight-passenger-overview.component';
+export * from './lib/ui-activity-overview/activity-overview.component';
---
+++ frontend/libs/statistics/ui/src/lib/ui-activity-overview/activity-overview.component.ts
@@ -0,0 +1,26 @@
+import { Component, input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SliderCardComponent } from '@shared/ui-components';
+import { NgxEchartsDirective, provideEcharts } from 'ngx-echarts';
+
+@Component({
+  selector: 'statistics-activity-overview',
+  standalone: true,
+  imports: [CommonModule, SliderCardComponent, NgxEchartsDirective],
+  template: ` <shared-slider-card style="display: flex; justify-content: center; margin-top: 50px">
+    <div class="card" *ngFor="let statistic of activityOverview()">
+      <img class="card__image" [src]="statistic.imageSrc" />
+      <div class="card__title">
+        <p>{{ statistic.cardTitle }}</p>
+      </div>
+      <div class="card__info-container">
+        <div echarts [options]="statistic"></div>
+      </div>
+    </div>
+  </shared-slider-card>`,
+  styleUrl: './activity-overview.component.scss',
+  providers: [provideEcharts()],
+})
+export class ActivityOverviewComponent {
+  activityOverview = input.required<any>();
+}
