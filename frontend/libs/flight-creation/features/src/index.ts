4d00a849fcbd9753b405a9aebd169f6cb8e26a92
patrykluszcz <patryk.luszcz@outlook.com>
2024-11-14 20:49:08 +0100

    feat: create features library in flight creation domain

---
+++ frontend/libs/flight-creation/features/src/index.ts
@@ -0,0 +1,1 @@
+export * from './lib/flight-creation-container.component';
---
+++ frontend/libs/flight-creation/features/src/lib/flight-creation-container.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  standalone: true,
+  imports: [CommonModule],
+  template: `<p>flight-creation-container works!</p>`,
+  styleUrl: './flight-creation-container.component.scss',
+})
+export class FlightCreationContainerComponent {}
