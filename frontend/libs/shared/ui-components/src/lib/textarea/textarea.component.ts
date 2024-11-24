f0117cb19788ef455f56642ea9925f4129b87463
patrykluszcz <patryk.luszcz@outlook.com>
2024-11-25 00:44:11 +0100

    feat: implement shared textarea component

---
+++ frontend/libs/shared/ui-components/src/lib/textarea/textarea.component.ts
@@ -0,0 +1,41 @@
+import { Component, forwardRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InputComponent } from '../input/input.component';
+import { ValidationSignComponent } from '../validation-sign/validation-sign.component';
+import { NG_VALUE_ACCESSOR } from '@angular/forms';
+
+@Component({
+  selector: 'shared-textarea',
+  standalone: true,
+  imports: [CommonModule, ValidationSignComponent],
+  template: `<label for="textarea">{{ label() }}</label>
+
+    <div id="textarea" class="textarea-container">
+      <textarea
+        class="textarea"
+        [placeholder]="placeholder()"
+        (input)="setInput($event.target)"
+        (blur)="onTouched()"
+        type="text"
+        autocomplete="off"
+        role="presentation"
+      ></textarea>
+
+      @if(formField){
+      <shared-validation-sign [formField]="formField"></shared-validation-sign>
+      }
+    </div> `,
+  styleUrl: './textarea.component.scss',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => TextareaComponent),
+      multi: true,
+    },
+  ],
+})
+export class TextareaComponent extends InputComponent {
+  constructor() {
+    super();
+  }
+}
