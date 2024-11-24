42ebabf181a6ad8c3c997f4fd9701769363a61f7
patrykluszcz <patryk.luszcz@outlook.com>
2024-11-24 17:13:17 +0100

    feat: add created guards inot flight creation routes configuration

--- frontend/libs/flight-creation/routes/src/lib/flight-creation.routes.ts
+++ frontend/libs/flight-creation/routes/src/lib/flight-creation.routes.ts
@@ -1,9 +1,30 @@
 import { Route } from '@angular/router';
 import { FlightCreationContainerComponent } from '@flight-saver/flight-creation/features';
+import { FlightFormComponent, TicketFormComponent } from '@flight-saver/flight-creation/ui';
+import { flightFormGuard } from './flight-form.guard';
+import { ticketFormGuard } from './ticket-form.guard';
+import { flightCreationGuard } from './flight-creation.guard';
 
 export const flightCreationRoutes: Route[] = [
   {
     path: '',
     component: FlightCreationContainerComponent,
+    canActivate: [flightCreationGuard],
+    children: [
+      {
+        path: 'flight',
+        component: FlightFormComponent,
+        canDeactivate: [flightFormGuard],
+      },
+      {
+        path: 'ticket',
+        component: TicketFormComponent,
+        canDeactivate: [ticketFormGuard],
+      },
+      {
+        path: 'rate-and-review',
+        component: FlightFormComponent,
+      },
+    ],
   },
 ];
