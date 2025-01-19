2556bd2a259158c4ea2362df9fadc081fcda963d
patrykluszcz <patryk.luszcz@outlook.com>
2025-01-19 20:45:30 +0100

    feat: add shared loader selector to app component template

--- frontend/apps/flight-saver/src/app/app.component.ts
+++ frontend/apps/flight-saver/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { LoaderComponent } from '@shared/ui-components';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule],
-  template: `<router-outlet></router-outlet>`,
+  imports: [RouterModule, LoaderComponent],
+  template: `<shared-loader></shared-loader> <router-outlet></router-outlet>`,
 })
 export class AppComponent {}
