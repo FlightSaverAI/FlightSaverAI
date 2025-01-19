84011ebc4dc94b4e676f7d3b33e2b420e8deea70
patrykluszcz <patryk.luszcz@outlook.com>
2025-01-19 13:58:16 +0100

    feat: Retrieve flight statistics and details from the backend and render them on the map and statistics panel.

--- frontend/libs/home/ui/src/lib/ui-flights-summary/flights-summary.component.ts
+++ frontend/libs/home/ui/src/lib/ui-flights-summary/flights-summary.component.ts
@@ -1,37 +1,63 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormatTimePipe } from './format-time.pipe';
 
 @Component({
   selector: 'home-flights-summary',
   standalone: true,
-  imports: [CommonModule],
-  template: ` <!-- MOCKED DATA -->
-    <div class="summary">
-      <div class="summary__flights">
-        <img src="global/assets/flight.svg" alt="" />
-        <div class="summary__stats">
-          <p><strong>12</strong> flights</p>
-          <p><strong>8</strong> international</p>
-          <p><strong>4</strong> domestic</p>
-        </div>
+  imports: [CommonModule, FormatTimePipe],
+  template: `<div class="summary">
+    <div class="summary__flights">
+      <img src="global/assets/flight.svg" alt="" />
+      <div class="summary__stats">
+        <p>
+          <strong>{{ this.statistics().flightCount.count }}</strong> flights
+        </p>
+        <p>
+          <strong>{{ this.statistics().flightCount.flightDistribution.International }}</strong>
+          international
+        </p>
+        <p>
+          <strong>{{ this.statistics().flightCount.flightDistribution.Domestic }}</strong>
+          domestic
+        </p>
       </div>
-      <div class="summary__flights">
-        <img src="global/assets/distance.svg" alt="" />
-        <div class="summary__stats">
-          <p><strong>5670</strong> km</p>
-          <p><strong>0,14x</strong> around the earth</p>
-          <p><strong>0,1x</strong> to the moon</p>
-        </div>
+    </div>
+    <div class="summary__flights">
+      <img src="global/assets/distance.svg" alt="" />
+      <div class="summary__stats">
+        <p>
+          <strong>{{ totalDistance() }}</strong> km
+        </p>
+        <p>
+          <strong>{{ this.statistics().distance.aroundEarthDistance.toFixed(2) }}x</strong> around
+          the earth
+        </p>
+        <p>
+          <strong>{{ this.statistics().distance.toTheMoonDistance.toFixed(2) }}x</strong> to the
+          moon
+        </p>
       </div>
-      <div class="summary__flights">
-        <img src="global/assets/time.svg" alt="" />
-        <div class="summary__stats">
-          <p><strong>29h 35 min</strong></p>
-          <p><strong>1.23</strong> days</p>
-          <p><strong>0.4</strong> months</p>
-        </div>
+    </div>
+    <div class="summary__flights">
+      <img src="global/assets/time.svg" alt="" />
+      <div class="summary__stats">
+        <p>
+          <strong>{{ this.statistics().totalFlightTime.time | formatTime }}</strong>
+        </p>
+        <p>
+          <strong>{{ this.statistics().totalFlightTime.days.toFixed(2) }}</strong> days
+        </p>
+        <p>
+          <strong>{{ this.statistics().totalFlightTime.months.toFixed(2) }}</strong> months
+        </p>
       </div>
-    </div>`,
+    </div>
+  </div>`,
   styleUrl: './flights-summary.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FlightsSummaryComponent {}
+export class FlightsSummaryComponent {
+  statistics = input.required<any>();
+  totalDistance = computed(() => Math.round(this.statistics().distance.totalDistance));
+}
