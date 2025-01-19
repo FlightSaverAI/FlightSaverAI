85cc698aecfb9b41ab252ac45e5b638272d6f7f2
patrykluszcz <patryk.luszcz@outlook.com>
2025-01-19 01:07:16 +0100

    feat: export created components

--- frontend/libs/shared/ui-components/src/index.ts
+++ frontend/libs/shared/ui-components/src/index.ts
@@ -5,3 +5,5 @@ export * from './lib/slider-card/slider-card.component';
 export * from './lib/textarea/textarea.component';
 export * from './lib/star-rating/star-rating.component';
 export * from './lib/page-not-found/page-not-found.component';
+export * from './lib/select/select.component';
+export * from './lib/date-picker/date-picker.component';
