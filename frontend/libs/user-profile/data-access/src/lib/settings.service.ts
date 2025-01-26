1832e37d92090d24bad728d6b3803b5ebb8140dc
patrykluszcz <patryk.luszcz@outlook.com>
2025-01-26 19:16:28 +0100

    feat: implement logic to get and update user data

---
+++ frontend/libs/user-profile/data-access/src/lib/settings.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '@environments/environments';
+
+@Injectable()
+export class SettingsService {
+  private _http = inject(HttpClient);
+
+  public getUserProfileData() {
+    return this._http.get<any>(`${environment.url}/user/info`);
+  }
+
+  public updateUserProfileData(form: any) {
+    return this._http.put<any>(`${environment.url}/user`, form);
+  }
+}
