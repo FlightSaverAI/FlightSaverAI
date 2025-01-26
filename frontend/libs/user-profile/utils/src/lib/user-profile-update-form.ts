4a3fe5a4e60a81bf6f3e6bffa5ab9957c083e70d
patrykluszcz <patryk.luszcz@outlook.com>
2025-01-26 20:35:37 +0100

    feat: create user profile form

--- frontend/libs/user-profile/utils/src/lib/user-profile-update-form.ts
+++ frontend/libs/user-profile/utils/src/lib/user-profile-update-form.ts
@@ -1,20 +1,11 @@
 import { inject } from '@angular/core';
 import { NonNullableFormBuilder, Validators } from '@angular/forms';
-//TO_FIX
-import { passwordMatchValidator } from '@flight-saver/authentication/utils';
 
-export type RegistrationForm = ReturnType<typeof userProfileUpdateForm>;
+export type UserProfileUpdateForm = ReturnType<typeof userProfileUpdateForm>;
 
 export function userProfileUpdateForm() {
-  return inject(NonNullableFormBuilder).group(
-    {
-      username: [''],
-      email: ['', [Validators.email]],
-      password: [''],
-      confirmPassword: ['', Validators.required],
-    },
-    {
-      validators: passwordMatchValidator('password', 'confirmPassword'),
-    }
-  );
+  return inject(NonNullableFormBuilder).group({
+    username: [''],
+    email: ['', [Validators.email]],
+  });
 }
