8546d59b55913df8bcba97329e4523f2d5757e92
patrykluszcz <patryk.luszcz@outlook.com>
2025-02-04 23:56:57 +0100

    ref: loader interceptor code refactoring - increase delay timer to 100ms

--- frontend/libs/shared/data-access/src/lib/loader.interceptor.ts
+++ frontend/libs/shared/data-access/src/lib/loader.interceptor.ts
@@ -1,17 +1,20 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { timer } from 'rxjs';
-import { finalize, switchMap } from 'rxjs/operators';
+import { finalize } from 'rxjs/operators';
 import { LoaderService } from './loader.service';
 
 export const loaderInterceptor: HttpInterceptorFn = (req, next) => {
   const _loaderService = inject(LoaderService);
-  const showLoader = timer(10).pipe(
-    switchMap(() => {
-      _loaderService.show();
-      return next(req);
+
+  const delayTimer = timer(100).subscribe(() => {
+    _loaderService.show();
+  });
+
+  return next(req).pipe(
+    finalize(() => {
+      _loaderService.hide();
+      delayTimer.unsubscribe();
     })
   );
-
-  return showLoader.pipe(finalize(() => _loaderService.hide()));
 };
