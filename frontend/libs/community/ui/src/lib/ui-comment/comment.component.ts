854b6f0479793ad25977fe7633e6b60b28113f75
patrykluszcz <patryk.luszcz@outlook.com>
2025-02-07 22:19:20 +0100

    feat: update comment component to use user profile picture and display data received from backend

--- frontend/libs/community/ui/src/lib/ui-comment/comment.component.ts
+++ frontend/libs/community/ui/src/lib/ui-comment/comment.component.ts
@@ -7,14 +7,14 @@ import { NgOptimizedImage } from '@angular/common';
   standalone: true,
   imports: [CommonModule, NgOptimizedImage],
   template: `<div class="comment-container">
-    <img class="image" [ngSrc]="comment().photo" alt="" width="40" height="40" />
+    <img class="image" [ngSrc]="comment().user.profilePictureUrl" alt="" width="40" height="40" />
     <div class="u-w-100">
       <div class="comment">
-        <span class="comment__userName">{{ comment().userName }}</span>
-        <p class="comment__content">{{ comment().comment }}</p>
+        <span class="comment__userName">{{ comment().user.username }}</span>
+        <p class="comment__content">{{ comment().content }}</p>
       </div>
       <div class="comment__actions u-w-100">
-        <span>{{ comment().time }}</span>
+        <span>{{ comment().postedAt | date: 'dd MMM yyyy, HH:mm:ss' }}</span>
         <span>Like</span>
         <span>Reply</span>
       </div>
