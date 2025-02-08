f94656438bb3842ecb62fd67a845568b485e484d
patrykluszcz <patryk.luszcz@outlook.com>
2025-02-09 00:50:46 +0100

    feat: create friends domain and routes library

---
+++ frontend/libs/friends/routes/src/lib/friends.routes.ts
@@ -0,0 +1,9 @@
+import { Route } from '@angular/router';
+import { FriendsSearchComponent } from '@flight-saver/friends/features';
+
+export const friendsRoutes: Route[] = [
+  {
+    path: '',
+    component: FriendsSearchComponent,
+  },
+];
