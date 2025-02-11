df2418188e673b45e0cc4abaed03f30fba3808f8
patrykluszcz <patryk.luszcz@outlook.com>
2025-02-11 23:48:20 +0100

    feat: update authentication actions and effects to use CurrentUser model

--- frontend/libs/authentication/data-access/src/lib/store/auth.actions.ts
+++ frontend/libs/authentication/data-access/src/lib/store/auth.actions.ts
@@ -1,5 +1,5 @@
 import { createActionGroup, props } from '@ngrx/store';
-import { LoginFormData } from '@flight-saver/authentication/models';
+import { CurrentUser, LoginFormData } from '@flight-saver/authentication/models';
 import { RegistrationFormData } from '@flight-saver/authentication/models';
 
 export const authActions = createActionGroup({
@@ -7,7 +7,7 @@ export const authActions = createActionGroup({
   events: {
     register: props<{ registerFormData: RegistrationFormData }>(),
     login: props<{ loginFormData: LoginFormData }>(),
-    loginSuccess: props<{ response: any }>(),
+    loginSuccess: props<{ currentUser: CurrentUser }>(),
     loginError: props<{ error: string }>(),
   },
 });
