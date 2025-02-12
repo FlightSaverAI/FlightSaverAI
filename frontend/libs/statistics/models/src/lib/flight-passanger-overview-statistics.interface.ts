77622fece6e3da37a790587d1be9f83a5ec48576
patrykluszcz <patryk.luszcz@outlook.com>
2025-02-12 22:30:42 +0100

    ref: update FlightPassengerOverviewStatistics interface to use shared NameValue model

--- frontend/libs/statistics/models/src/lib/flight-passanger-overview-statistics.interface.ts
+++ frontend/libs/statistics/models/src/lib/flight-passanger-overview-statistics.interface.ts
@@ -1,11 +1,8 @@
+import { NameValue } from '@shared/models';
+
 export interface FlightPassengerOverviewStatistics {
   classDistribution: NameValue[];
   seatDistribution: NameValue[];
   reasonDistribution: NameValue[];
   continents: NameValue[];
-}
-
-interface NameValue {
-  name: string;
-  value: number;
 }
