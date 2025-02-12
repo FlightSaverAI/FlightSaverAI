a12358428ef367fcefe8dd974a2ec4b4d725a6a6
patrykluszcz <patryk.luszcz@outlook.com>
2025-02-12 22:31:02 +0100

    feat: add chart configuration interfaces for activity and top overview statistics

--- frontend/libs/statistics/models/src/index.ts
+++ frontend/libs/statistics/models/src/index.ts
@@ -1,3 +1,5 @@
 export * from './lib/top-overview-statistics.interface';
 export * from './lib/flight-passanger-overview-statistics.interface';
 export * from './lib/activity-overview-statistics.interface';
+export * from './lib/activity-overview-chart-config.interface';
+export * from './lib/top-overview-chart-config.interface';
