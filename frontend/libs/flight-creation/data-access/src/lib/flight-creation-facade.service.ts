b39ecc48b4d444e878e8b0eed8e08191ecc71715
patrykluszcz <patryk.luszcz@outlook.com>
2025-02-16 02:29:45 +0100

    hotfix: add success alert after flight addition

--- frontend/libs/flight-creation/data-access/src/lib/flight-creation-facade.service.ts
+++ frontend/libs/flight-creation/data-access/src/lib/flight-creation-facade.service.ts
@@ -1,16 +1,21 @@
 import { inject, Injectable } from '@angular/core';
 import { FlightFormService } from './flight-form.service';
 import { Router } from '@angular/router';
+import { AlertService } from '@shared/data-access';
 
 @Injectable()
 export class FlightCreationFacadeService {
   private _flightForm = inject(FlightFormService);
+  private _alertService = inject(AlertService);
   private _router = inject(Router);
   private _homeUrl = '/authorized/home';
 
   public saveFlight(payload: any) {
     this._flightForm.addFlight(payload).subscribe({
-      next: () => this._router.navigateByUrl(this._homeUrl),
+      next: () => {
+        this._router.navigateByUrl(this._homeUrl);
+        this._alertService.showAlert('success', 'Flight has been added successfuly');
+      },
       error: (error) => alert(error),
     });
   }
